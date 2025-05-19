8d4c8f08238b9ffa823895ac5bf0448063fd6520
jitendra choudhary <jitu@jitendras-MacBook-Air.local>
2025-05-19 18:40:53 +0530

    added logger

--- src/pkg/service/service.go
+++ src/pkg/service/service.go
@@ -1,13 +1,13 @@
-package service 
+package service
 
 import (
+	"hisaab-kitaab/pkg/logger"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (s *serviceObj) Status(c *gin.Context) {
-
+	logger.Log().Info("Status API called")
 	c.String(http.StatusOK, "Working!")
 }
-
